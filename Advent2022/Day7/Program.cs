8b978a4e3505181597cdecb7dcc173867d2678e9
Kevin Day <kday@up.com>
2022-12-10 23:21:51 -0600

    Day 7

---
+++ Advent2022/Day7/Program.cs
@@ -0,0 +1,59 @@
+﻿using static Common.Common;
+using Day7;
+
+TreeNode parse(string file_name)
+{
+    List<string> input = readInput(file_name);
+    TreeNode root = new TreeNode("/");
+    TreeNode? curr = null;
+
+    foreach (string line in input)
+    {
+        curr = line switch
+        {
+            "$ cd /" => root,
+            "$ cd .." => curr.parent,
+            _ when line.StartsWith("$ cd") => curr.children.First(c => c.name == line.Split(' ').Last()),
+            _ when line.StartsWith("dir ") => curr.addChild(new TreeNode(line.Split(' ').Last(), curr)),
+            _ when line != "$ ls" => curr.addChild(new TreeNode(line.Split(' ').Last(), curr, long.Parse(line.Split(' ').First()))),
+            _ => curr,
+        };
+    }
+
+    return root;
+}
+
+List<long> getDirSizes(TreeNode node)
+{
+    List<long> dir_sizes = new List<long>();
+
+    foreach (TreeNode child in node.children)
+    {
+        dir_sizes.AddRange(getDirSizes(child));
+    }
+
+    if (node.isDir())
+    {
+        dir_sizes.Add(node.TotalSize());
+    }
+
+    return dir_sizes;
+}
+
+void part1(string file_name)
+{
+    TreeNode root = parse(file_name);
+    Console.WriteLine(getDirSizes(root).Where(size => size <= 100000).Sum());
+}
+
+void part2(string file_name)
+{
+    TreeNode root = parse(file_name);
+    long need_to_free = root.TotalSize() - 70000000 + 30000000;
+    Console.WriteLine(getDirSizes(root).Where(size => size >= need_to_free).Min());
+}
+
+part1("sample_input.txt");
+part1("input.txt");
+part2("sample_input.txt");
+part2("input.txt");
---
+++ Advent2022/Day7/TreeNode.cs
@@ -0,0 +1,32 @@
+﻿namespace Day7
+{
+    internal class TreeNode
+    {
+        public string name;
+        public TreeNode? parent;
+        public List<TreeNode> children;
+
+        long size;
+
+        public TreeNode(string name, TreeNode? parent = null, long size = 0)
+        {
+            this.name = name;
+            this.parent = parent;
+            children = new List<TreeNode>();
+            this.size = size;
+        }
+
+        public TreeNode addChild(TreeNode node)
+        {
+            if(children.Where(n => n.name == node.name).Count() == 0)
+            {
+                children.Add(node);
+            }
+            return this;
+        }
+
+        public long TotalSize() => size + children.Sum(c => c.TotalSize());
+
+        public bool isDir() => size == 0;
+    }
+}
